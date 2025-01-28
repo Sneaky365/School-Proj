c4d460ad8e8c6c0a975f95e95731a39db0e6453a
Simeon <simo.banev@gmail.com>
2025-01-28 10:15:43 +0200

    grand finale

--- Main/Game/GameSpace.Designer.cs
+++ Main/Game/GameSpace.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameSpace));
             this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -42,69 +43,78 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(409, 74);
+            this.label1.Location = new System.Drawing.Point(545, 91);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.Size = new System.Drawing.Size(46, 16);
             this.label1.TabIndex = 1;
             this.label1.Text = "Score:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(409, 108);
+            this.label3.Location = new System.Drawing.Point(545, 133);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.Size = new System.Drawing.Size(74, 16);
             this.label3.TabIndex = 3;
             this.label3.Text = "Next piece:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(409, 247);
+            this.label2.Location = new System.Drawing.Point(545, 304);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.Size = new System.Drawing.Size(92, 16);
             this.label2.TabIndex = 5;
             this.label2.Text = "Holded piece:";
             // 
             // pictureBox3
             // 
             this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox3.Location = new System.Drawing.Point(412, 263);
+            this.pictureBox3.Location = new System.Drawing.Point(549, 324);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox3.Size = new System.Drawing.Size(132, 61);
             this.pictureBox3.TabIndex = 6;
             this.pictureBox3.TabStop = false;
             // 
             // pictureBox2
             // 
             this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox2.Location = new System.Drawing.Point(412, 124);
+            this.pictureBox2.Location = new System.Drawing.Point(549, 153);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.Size = new System.Drawing.Size(132, 61);
             this.pictureBox2.TabIndex = 4;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox1
             // 
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox1.Location = new System.Drawing.Point(65, 73);
+            this.pictureBox1.Location = new System.Drawing.Point(87, 90);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(300, 400);
+            this.pictureBox1.Size = new System.Drawing.Size(399, 491);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // GameSpace
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Silver;
-            this.ClientSize = new System.Drawing.Size(610, 524);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(813, 645);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "GameSpace";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.GameSpace_Load);
--- Main/HomePage/HomeForm.Designer.cs
+++ Main/HomePage/HomeForm.Designer.cs
@@ -44,9 +44,10 @@ partial class HomeForm
         button1.FlatStyle = FlatStyle.Flat;
         button1.Font = new Font("Comic Sans MS", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
         button1.ForeColor = Color.White;
-        button1.Location = new Point(241, 254);
+        button1.Location = new Point(275, 339);
+        button1.Margin = new Padding(3, 4, 3, 4);
         button1.Name = "button1";
-        button1.Size = new Size(319, 154);
+        button1.Size = new Size(365, 205);
         button1.TabIndex = 0;
         button1.Text = "Play";
         button1.UseVisualStyleBackColor = false;
@@ -62,9 +63,10 @@ partial class HomeForm
         button2.FlatStyle = FlatStyle.Flat;
         button2.Font = new Font("Comic Sans MS", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
         button2.ForeColor = Color.White;
-        button2.Location = new Point(48, 54);
+        button2.Location = new Point(55, 72);
+        button2.Margin = new Padding(3, 4, 3, 4);
         button2.Name = "button2";
-        button2.Size = new Size(161, 115);
+        button2.Size = new Size(184, 153);
         button2.TabIndex = 1;
         button2.Text = "View Account Info";
         button2.UseVisualStyleBackColor = false;
@@ -80,9 +82,10 @@ partial class HomeForm
         button3.FlatStyle = FlatStyle.Flat;
         button3.Font = new Font("Comic Sans MS", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
         button3.ForeColor = Color.White;
-        button3.Location = new Point(592, 65);
+        button3.Location = new Point(677, 87);
+        button3.Margin = new Padding(3, 4, 3, 4);
         button3.Name = "button3";
-        button3.Size = new Size(145, 92);
+        button3.Size = new Size(173, 123);
         button3.TabIndex = 2;
         button3.Text = "Leaderboard";
         button3.UseVisualStyleBackColor = false;
@@ -94,24 +97,25 @@ partial class HomeForm
         label3.BackColor = Color.Transparent;
         label3.Font = new Font("Comic Sans MS", 48F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
         label3.ForeColor = Color.White;
-        label3.Location = new Point(294, 9);
+        label3.Location = new Point(336, 12);
         label3.Name = "label3";
-        label3.Size = new Size(207, 90);
+        label3.Size = new Size(258, 113);
         label3.TabIndex = 9;
         label3.Text = "Home";
         // 
         // HomeForm
         // 
-        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleDimensions = new SizeF(8F, 20F);
         AutoScaleMode = AutoScaleMode.Font;
         BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
         BackgroundImageLayout = ImageLayout.Stretch;
-        ClientSize = new Size(800, 450);
+        ClientSize = new Size(914, 600);
         Controls.Add(label3);
         Controls.Add(button3);
         Controls.Add(button2);
         Controls.Add(button1);
         DoubleBuffered = true;
+        Margin = new Padding(3, 4, 3, 4);
         Name = "HomeForm";
         Text = "Form1";
         Activated += HomeForm_Activated;
